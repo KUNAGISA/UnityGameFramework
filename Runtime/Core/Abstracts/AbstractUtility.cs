66081f53f2f3c79ff379497fe2622b661a1d06f2
零 <kunagisatomo@live.com>
2022-05-06 15:38:22 +0800

    【Utility】干掉IDestory

--- Runtime/Core/Abstracts/AbstractUtility.cs
+++ Runtime/Core/Abstracts/AbstractUtility.cs
@@ -5,16 +5,12 @@ namespace Framework
     /// <summary>
     /// 工具基类
     /// </summary>
-    public abstract class AbstractUtility : IUtility, IDestory
+    public abstract class AbstractUtility : IUtility
     {
         private IArchitecture m_Architecture;
-
-        void IDestory.Destroy() => OnDestoryUtility();
 
         IArchitecture IBelongArchiecture.GetArchitecture() => m_Architecture;
 
         void ICanSetArchiecture.SetArchiecture(IArchitecture architecture) => m_Architecture = architecture;
-
-        abstract protected void OnDestoryUtility();
     }
 }
