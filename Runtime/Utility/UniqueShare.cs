ea387bb9c6b260d2f99ee85be0fe4dcc13e1b8f4
KUNAGISA <kunagisatomo@live.com>
2024-02-16 16:24:48 +0800

    新增UnuqueShare

---
+++ Runtime/Utility/UniqueShare.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Framework
+{
+    public struct UnuqueShare<T> : IDisposable where T : class, new()
+    {
+        private static T shareInstance = null;
+
+        public static void Clearup()
+        {
+            shareInstance = null;
+        }
+
+        private T m_keepInstance;
+
+        public UnuqueShare(out T value)
+        {
+            m_keepInstance = value = shareInstance ?? new T();
+            shareInstance = null;
+        }
+
+        public void Dispose()
+        {
+            shareInstance = m_keepInstance ?? shareInstance;
+            m_keepInstance = null;
+        }
+    }
+}
